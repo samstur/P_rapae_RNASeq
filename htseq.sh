3895dbdef5c21f19fbc313b70da47e1c37bdb5d4
samstur <96506496+samstur@users.noreply.github.com>
2023-09-09 13:30:06 -0400

    Create htseq.sh

---
+++ htseq.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=htseq_count --output=%x.%j.out
+#SBATCH --mail-type=END,FAIL --mail-user=sls366@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=96:00:00
+#SBATCH --mem=4G
+
+#-----------------------------------------------------------------------------#
+# This script uses htseq to count and assign reads to genes #
+#-----------------------------------------------------------------------------#
+
+source activate conda_env
+
+#- Set variables ----------------------------------------------------------------#
+
+bam_dir=/home/sls366/prapae/bam_dir
+count_dir=/home/sls366/prapae/counts_dir
+htseq=/home/sls366/.conda/envs/conda_env/bin/htseq-count
+ref=/home/sls366/prapae/genome_files/genomic.gff
+
+#- RUN htseq ----------------------------------------------------------------#
+
+files=(${bam_dir}/*_Aligned.out.bam)
+for file in ${files[@]}
+do
+base=`basename ${file} _Aligned.out.bam`
+${htseq} -f bam -r pos -s yes -t exon -i gene ${bam_dir}/${base}_Aligned.out.bam ${ref} > ${count_dir}/${base}_htseqCount
+
+done
+
+## -s yes indicates that our sequencing is forward stranded
+
+#- FIN -----------------------------------------------------------------------#
