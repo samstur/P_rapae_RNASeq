fb45d9d16e617cf0025a0abfcf69a1cd6ccb017a
samstur <96506496+samstur@users.noreply.github.com>
2025-11-11 11:22:37 -0500

    Update Prapae_Mortality_DevTime_Plot.R

--- Prapae_Mortality_DevTime_Plot.R
+++ Prapae_Mortality_DevTime_Plot.R
@@ -1,14 +1,7 @@
 
 
-setwd("/Users/samsturiale/Documents/Georgetown/Armbruster Lab/INTBio grant work/Writing/Input Files/Input files for Dev Mortality Script")
-data <- read.csv(file="cat_viral_records.csv")
-head(data)
-data$rearing_temp <- substr(data$Treatment, 1, 2) # this takes the first three letters of each sampleName to give the treatment name without the rep number
-data$SIB <- as.character(data$SIB)
-
-## subset only the caterpillars who were tested and had a clear result. Takes sample size from 120 to 108
-data_tested <- subset(data,data$inf_status == "positive" | data$inf_status == "negative")
-
+setwd("/Users/samsturiale/Documents/Georgetown/Armbruster Lab/INTBio grant work/Writing/Final Scripts/Script_outputfiles/Prapae_Mortality_DevTime_Plot_outputfiles")
+data <- read.csv(data_final,"lifehistory_data_final.csv",row.names = F)
 
 library("lme4")
 library("car")
@@ -16,12 +9,15 @@ library(predictmeans)
 library(gridExtra)
 library(ggplot2)
 
-View(data_tested)
+data$rearing_temp <- substr(data$Treatment, 1, 2) 
+data$testing_temp <- substr(data$Treatment, 4, 5)
+data$SIB <- as.factor(data$SIB)
+
 ## statistical analysis of dev time in caterpillar which were tested for the virus
-data_tested$days_to_fourth.log <- log(data_tested$days_to_fourth)
-data_tested$days_to_fourth.sqrt <- sqrt(data_tested$days_to_fourth)
+data$days_to_fourth.log <- log(data$days_to_fourth)
+data$days_to_fourth.sqrt <- sqrt(data$days_to_fourth)
 model1 <- lmer(days_to_fourth.sqrt ~ rearing_temp+inf_status + (1|SIB),
-               data = data_tested)
+               data = data)
 summary(model1)
 Anova(model1)
 df.residual(model1) #107
@@ -52,26 +48,9 @@ dev_plot
 
 ############# now for death data ######################
 
-setwd("/Users/samsturiale/Documents/Georgetown/Armbruster Lab/INTBio grant work/Writing/Input Files/Input files for Dev Mortality Script")
-datadead <- read.csv(file="fullcatdata.csv")
-datadead$SIB <- as.character(datadead$SIB)
-head(datadead)
-## get rid of blank rows
-datadead <- datadead[c(1:187),]
-
-## retain only individuals that reached 4th instar or died 
-datadead <- subset(datadead,datadead$fate == "4th" | datadead$fate == "pmd" )
-
-# this takes the first three letters of each sampleName 
-## to give the treatment name without the rep number
-datadead$rearing_temp <- substr(datadead$Treatment, 1, 2) 
-
-## add binomial death variable
-datadead$death <- with(datadead, ifelse(fate == "4th", 0, 1))
-
 library(ggplot2)
 lb1_dead <- c("DevTemp^ns")
-death_plot<-ggplot(datadead,aes(x=rearing_temp,y=death))+
+death_plot<-ggplot(data,aes(x=rearing_temp,y=death))+
   geom_quasirandom(size=0.5,alpha=0.3)+
   stat_summary(fun.data=mean_se,size=0.6)+
   stat_summary(fun.data = mean_se, geom = "errorbar", 
@@ -97,8 +76,9 @@ ggsave("devdeath_plot_testedcats_smallpoints.png",plot=grid.arrange(death_plot,d
        dpi=600,units='in',width=6,height=4.5)
 
 View(datadead)
+
 modeldead <- glmer(death ~ rearing_temp + (1|SIB),
-               data = datadead,family = binomial)
+               data = data,family = binomial)
 summary(modeldead)
 Anova(modeldead)
 df.residual(modeldead) #167
@@ -124,44 +104,18 @@ df_dead <- datadead %>%
 df_dead
 
 ## get totals of infected caterpillars for each treatment and test for a difference in inf across rearing temperatures
-data_tested$inf_status_yn <- with(data_tested, ifelse(inf_status == "negative", 0, 1))
-data_tested %>% count(rearing_temp, inf_status, sort = TRUE)
+data$inf_status_yn <- with(data, ifelse(inf_status == "negative", 0, 1))
+data %>% count(rearing_temp, inf_status, sort = TRUE)
 modelinf <- glmer(inf_status_yn ~ rearing_temp + (1|SIB),
-                   data = data_tested,family = binomial)
+                   data = data,family = binomial)
 summary(modelinf)
 Anova(modelinf)
 df.residual(modelinf) #109
 residplot(modelinf) 
 
-
-table(datadead$rearing_temp)
-########### combine death data and viral infection data for a final data file to upload with paper
-head(data_tested)
-data_tested$unique_ID <- paste(data_tested$SIB,"-",data_tested$ID)
-head(datadead)
-datadead$unique_ID <- paste(datadead$SIB,"-",datadead$ID)
-View(data_tested)
-data_tested_subset <- data_tested[,c(14,19)]
-
-## need to add testing data to all the datadead rows (or unknown if not tested)
-
-data_final <- merge(datadead,data_tested_subset,by="unique_ID",all=T)
-data_final$Treatment_new <- ifelse(data_final$Treatment == "30C", '30-30',
-                                   ifelse(data_final$Treatment == "30F", '30-16',
-                                          ifelse(data_final$Treatment == "16C", '16-16',
-                                                 ifelse(data_final$Treatment == "16F", '16-30',NA))))
-data_final <- data_final[,c(1,2,18,3,4,5,16,10,14,17)]
-View(data_final)
-data_final$unique_ID <- as.character(data_final$unique_ID)
-
-setwd("/Users/samsturiale/Documents/Georgetown/Armbruster Lab/INTBio grant work/Writing/Final Scripts/Script_outputfiles/Prapae_Mortality_DevTime_Plot_outputfiles")
-write.csv(data_final,"lifehistory_data_final.csv",row.names = F)
-
-data <- read.csv("lifehistory_data_final.csv")
-View(data)
-
+## get counts of sib families across treatments
 library(dplyr)
-table <- data %>% count(SIB, Treatment_new)
+table <- data %>% count(SIB, Treatment)
 View(table)
 setwd("/Users/samsturiale/Documents/Georgetown/Armbruster Lab/INTBio grant work/Writing/Final Scripts/Script_outputfiles/Prapae_Mortality_DevTime_Plot_outputfiles")
 write.csv(table,"SIB_numbers_table.csv",row.names = F)
